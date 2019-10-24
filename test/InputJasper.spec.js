a1a429a594392ca588e7d3c0a10821d6cbf0e6e6
Angelos Anagnostopoulos <angelos@anagnostic.org>
2019-10-24 21:27:34 +0300

    Add more tests

--- test/InputJasper.spec.js
+++ test/InputJasper.spec.js
@@ -17,5 +17,25 @@ describe('InputJasper', () => {
         // wrapper.setData({value: value})
         expect(wrapper.vm.inputVal).toBe(value)
     })
+    test("it emits the value when changed", () => {
+        const value = 'valuezzzz'
+        const wrapper = shallowMount(InputJasper, {
+            localVue: localVue,
+            propsData: { value }
+        })
+        wrapper.vm.changeVal(value)
+        expect(wrapper.emitted().change).toBeTruthy()
+        expect(wrapper.emitted().change.length).toBe(1)
+        expect(wrapper.emitted().change[0]).toEqual([{ name: undefined, value: value }])
+    })
+    test("it emits the value when typed", () => {
+        const value = 'valuezzzz'
+        const wrapper = shallowMount(InputJasper, {
+            localVue: localVue,
+            propsData: { value }
+        })
+        wrapper.vm.$options.watch.inputVal.call(wrapper.vm, value)
+        expect(wrapper.emitted("input")[0][0]).toBe(value)
+    })
 })
 
